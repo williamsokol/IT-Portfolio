ba48f11257c729003c9081bd9ebff10280d8510d
William <williamsokol0@gmail.com>
2026-01-26 11:30:56 -0800

    fix2

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-// Define __dirname manually
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: '/your-repo-name/', // Don't forget to update this!
   plugins: [react()],
   resolve: {
     alias: {
